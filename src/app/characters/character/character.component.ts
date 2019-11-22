9d989e55749ac70cbb2ffc015cdb85d3ef594c8c
regue <jacobo@hotmail.es>
2019-11-22 17:57:23 +0100

    rrr

--- src/app/characters/character/character.component.ts
+++ src/app/characters/character/character.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 
 @Component({
@@ -8,12 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CharacterComponent implements OnInit {
 
+  @Input() character: any ;
   constructor() { }
   
 
-  ngOnInit() {
-   
-    
+  ngOnInit() {  
   }
 
   
