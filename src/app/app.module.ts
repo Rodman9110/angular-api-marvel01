a28b5b829da68d939d05210f3ab1f19251783e2b
regue <jacobo@hotmail.es>
2019-11-22 17:14:42 +0100

    actualizar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,20 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule } from '@angular/material/toolbar';
+// import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
+// import { MatSidenavModule } from '@angular/material/sidenav';
+// import { MatIconModule } from '@angular/material/icon';
+// import { MatListModule } from '@angular/material/list';
 import { CharacterComponent } from './characters/character/character.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CharactersComponent } from './characters/characters.component';
 import { FlexLayoutModule } from "@angular/flex-layout";
-import { MatFormField, MatCardModule,  } from "@angular/material";
-import { MatFormFieldModule } from "@angular/material/form-field";
+// import { MatCardModule  } from "@angular/material";
+// import { MatFormFieldModule } from "@angular/material/form-field";
+// import { MatFormField } from "@angular/material/form-field";
 
+import { MatCardModule, MatToolbarModule, MatSidenavModule, MatIconModule, MatListModule, MatFormFieldModule  } from "@angular/material";
 
 @NgModule({
   declarations: [
@@ -39,7 +41,6 @@ import { MatFormFieldModule } from "@angular/material/form-field";
     MatListModule,
     HttpClientModule,
     FlexLayoutModule,
-    MatIconModule,
     // MatFormField,
     MatCardModule,
     MatFormFieldModule
